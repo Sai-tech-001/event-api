d32c6a668c07c8b026ca0252351fa16f930a35f4
Sai-tech-001 <saiganeshp66@gmail.com>
2025-09-15 14:14:10 +0530

    first commit

--- server.js
+++ server.js
@@ -5,16 +5,4 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello from event API!');
-});
-
-// example file read with error handling
-fs.readFile('data.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('File read error:', err);
-    } else {
-        console.log('file data:', data);
-    }
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Listening on', PORT));
+})
